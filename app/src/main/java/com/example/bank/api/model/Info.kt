df7934aee7dd3fa4e1d4c00a29e45bb71085e32e
vsls <vasilisa_001@mail.ru>
2022-12-24 21:34:12 +0600

    models for moshiConverter

---
+++ app/src/main/java/com/example/bank/api/model/Info.kt
@@ -0,0 +1,36 @@
+package com.example.bank.api.model
+
+
+import com.example.bank.Bin
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Info(
+    @Json(name = "bank")
+    val bank: Bank,
+    @Json(name = "brand")
+    val brand: String,
+    @Json(name = "country")
+    val country: Country,
+    @Json(name = "prepaid")
+    val prepaid: Boolean,
+    @Json(name = "scheme")
+    val scheme: String,
+    @Json(name = "type")
+    val type: String,
+    @Json(name = "number")
+    val number: Number
+) {
+    fun toBin(): Bin = Bin(
+        brand = brand,
+        country = country.currency,
+        prepaid = prepaid,
+        scheme = scheme,
+        type = type,
+        cityBank = bank.city,
+        nameBank = bank.name,
+        phoneBank = bank.phone,
+        urlBank = bank.url
+    )
+}

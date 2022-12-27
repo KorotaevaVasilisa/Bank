7e4ff333a7e6e21da08b46f0d4248cb1862da684
vsls <vasilisa_001@mail.ru>
2022-12-27 21:28:40 +0600

    added Room and entity

---
+++ app/src/main/java/com/example/bank/database/BinEntity.kt
@@ -0,0 +1,31 @@
+package com.example.bank.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class BinEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo(name = "bin")
+    val bin: String,
+    @ColumnInfo(name = "brand")
+    val brand: String,
+    @ColumnInfo(name = "country")
+    val country: String,
+    @ColumnInfo(name = "prepaid")
+    val prepaid: Boolean = true,
+    @ColumnInfo(name = "scheme")
+    val scheme: String,
+    @ColumnInfo(name = "type")
+    val type: String,
+    @ColumnInfo(name = "city_bank")
+    val cityBank: String,
+    @ColumnInfo(name = "name_bank")
+    val nameBank: String,
+    @ColumnInfo(name = "phone_bank")
+    val phoneBank: String,
+    @ColumnInfo(name = "url_bank")
+    val urlBank: String,
+)

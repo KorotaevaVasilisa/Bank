230d19c7c2c8bf75a4a3cffbd26289c4a812c1d0
vsls <vasilisa_001@mail.ru>
2022-12-28 14:35:28 +0600

    get and insert data (in ViewModel with factory)

--- app/src/main/java/com/example/bank/screen/MainViewModel.kt
+++ app/src/main/java/com/example/bank/screen/MainViewModel.kt
@@ -1,25 +1,40 @@
 package com.example.bank.screen
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.bank.Bin
-import com.example.bank.api.BankApi
+import com.example.bank.Repository
+import com.example.bank.database.BinEntity
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+import java.net.ConnectException
+import java.net.SocketException
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val retrofitRepository: Repository,
+) : ViewModel() {
 
-    private val _bin = MutableStateFlow<Bin>(Bin())
-    val bin: StateFlow<Bin> = _bin.asStateFlow()
+    private val _bin = MutableStateFlow<BinEntity>(BinEntity())
+    val bin: StateFlow<BinEntity> = _bin.asStateFlow()
 
-    fun getBin(bin: String) {
+    fun getBin(binInput: String) {
         viewModelScope.launch {
             try {
-                _bin.value = BankApi.retrofitService.getBin(bin = bin).toBin()
-            } catch (e: Exception) {
-                print("Ошибка запроса")
+                _bin.value = retrofitRepository.getBin(bin = binInput).toBin(binInput)
+                retrofitRepository.insertBin(bin.value)
+
+            } catch (e: ConnectException) {
+                Log.e("RETROFIT", "ERROR 1: " + e.localizedMessage)
+            } catch (e: SocketException) {
+                Log.e("RETROFIT", "ERROR 2: " + e.localizedMessage)
+            } catch (e: IOException) {
+                Log.e("RETROFIT", "ERROR 3: " + e.localizedMessage)
+            } catch (e: HttpException) {
+                Log.e("RETROFIT", "ERROR 4: " + e.localizedMessage)
             }
         }
     }
